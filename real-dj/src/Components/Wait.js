c644666fcc735a26bbdaa449e9c6b32530cb23cf
ShindanoSingi <shindanosingi@sekur.com>
2022-12-18 23:55:12 -0500

    fixed Busy page

--- real-dj/src/Components/Wait.js
+++ real-dj/src/Components/Wait.js
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
+import * as React from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
 import './Wait.css'
 
-
 const Wait = () => {
-     return (
-
-          <div className='busy'>
-               <h1>Please wait...</h1>
-               <CircularProgress disableShrink/>
-          </div>
-     )
+  return (
+    <div className='busy-container'>
+	<div className="busy">
+	  <h1>Please wait...</h1>
+	  <CircularProgress disableShrink />
+	</div>
+    </div>
+  )
 }
 
 export default Wait
